28b9bd8f57e95609625f929f7a1e4499da476825
vauclasn <vauclasn@skysoft-atm.com>
2024-03-12 14:49:06 +0100

    add load_best_model_at_end, move to 1k samples

--- seb.sh
+++ seb.sh
@@ -10,15 +10,17 @@ python src/train_bash.py \
   --dataset ultra_chat \
   --dataset_dir data \
   --overwrite_cache \
-  --max_samples 100 \
+  --max_samples 1000 \
   --val_size 0.1 \
   --output_dir tmp/out/output/"$run_name" \
   --overwrite_output_dir \
   --do_train \
   --do_eval \
-  --evaluation_strategy epoch \
+  --evaluation_strategy steps \
   --logging_dir tmp/out/tensorboard/"$run_name" \
+  --save_strategy steps \
   --bf16 \
+  --load_best_model_at_end \
   --report_to tensorboard \
   --lora_target q_proj,k_proj,v_proj,o_proj,gate_proj,up_proj,down_proj
 
