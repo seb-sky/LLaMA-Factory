85f4748499b1a1b16f87be7564b02753723489a0
vauclasn <vauclasn@skysoft-atm.com>
2024-03-13 09:53:11 +0100

    log to file, remove --max_samples

--- seb.sh
+++ seb.sh
@@ -1,4 +1,6 @@
 run_name=seb-"$(date -u +'%FT%H%MZ')"
+out_dir=tmp/out/"$run_name"
+mkdir -p "$out_dir"
 
 python src/train_bash.py \
   --bf16 \
@@ -9,15 +11,14 @@ python src/train_bash.py \
   --do_train \
   --eval_steps 0.1 \
   --evaluation_strategy steps \
-  --export_dir tmp/out/export \
+  --export_dir "$out_dir"/export \
   --flash_attn \
   --load_best_model_at_end \
-  --logging_dir tmp/out/tensorboard/"$run_name" \
+  --logging_dir "$out_dir" \
   --logging_steps 0.01 \
   --lora_target q_proj,k_proj,v_proj,gate_proj,up_proj,down_proj,o_proj \
-  --max_samples 10000 \
   --model_name_or_path google/gemma-7b-it \
-  --output_dir tmp/out/output/"$run_name" \
+  --output_dir "$out_dir"/output \
   --overwrite_cache \
   --overwrite_output_dir \
   --quantization_bit 4 \
@@ -27,4 +28,5 @@ python src/train_bash.py \
   --template gemma \
   --upcast_layernorm \
   --use_unsloth \
-  --val_size 0.1
+  --val_size 0.1 \
+  |& tee "$out_dir"/log.txt
