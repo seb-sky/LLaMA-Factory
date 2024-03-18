0b4773c6159652fd37ee02cd7fa6ad6d2a29c36e
vauclasn <vauclasn@skysoft-atm.com>
2024-03-18 13:50:13 +0100

    latest predict

--- seb-predict.sh
+++ seb-predict.sh
@@ -5,7 +5,7 @@ mkdir -p "$out_dir"
 cp "$BASH_SOURCE" "$out_dir"/script.sh
 
 python src/train_bash.py \
-  --adapter_name_or_path /home/vauclasn/LLaMA-Factory/tmp/out/seb-2024-03-13T0857Z/output \
+  --adapter_name_or_path /home/vauclasn/LLaMA-Factory/tmp/out/seb-2024-03-14T1053Z/output \
   --bf16 \
   --dataset skyguidetest_test \
   --dataset_dir /home/vauclasn/parse_skyguidetest/output/dataset \
