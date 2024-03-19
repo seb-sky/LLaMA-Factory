18baab47a87dab06901b9ef09b45341466768b99
vauclasn <vauclasn@skysoft-atm.com>
2024-03-19 09:31:37 +0100

    try mistralai/Mistral-7B-Instruct-v0.2

--- seb-predict.sh
+++ seb-predict.sh
@@ -5,7 +5,7 @@ mkdir -p "$out_dir"
 cp "$BASH_SOURCE" "$out_dir"/script.sh
 
 python src/train_bash.py \
-  --adapter_name_or_path /home/vauclasn/LLaMA-Factory/tmp/out/seb-2024-03-18T1248Z/output \
+  --adapter_name_or_path /home/vauclasn/LLaMA-Factory/tmp/out/seb-2024-03-18T1546Z/output \
   --bf16 \
   --dataset skyguidetest_test \
   --dataset_dir /home/vauclasn/parse_skyguidetest/output/dataset \
@@ -13,11 +13,11 @@ python src/train_bash.py \
   --do_predict \
   --flash_attn \
   --lora_target q_proj,k_proj,v_proj,gate_proj,up_proj,down_proj,o_proj \
-  --model_name_or_path meta-llama/Llama-2-7b-chat-hf \
+  --model_name_or_path mistralai/Mistral-7B-Instruct-v0.2 \
   --output_dir "$out_dir"/output \
   --predict_with_generate \
   --quantization_bit 4 \
-  --template llama2 \
+  --template mistral \
   --upcast_layernorm \
   --use_unsloth \
   |& tee "$out_dir"/log.txt
